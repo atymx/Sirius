221e72c7ce45e2a8682ba11282ceebe5c755b3ec
Maksim <macsim.1999@gmail.com>
2018-11-10 19:00:38 +0300

    all events (without api)

--- Sirius/Controller/EventsViewController.swift
+++ Sirius/Controller/EventsViewController.swift
@@ -27,17 +27,60 @@ class EventsViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "toMyEvent" {
+            if let destination = segue.destination as? MyEventViewController {
+                destination.event = (sender as! Event)
+            }
+        }
     }
-    */
 
 }
 
+extension EventsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.numberOfLines = 0
 
+        let attributedString = NSMutableAttributedString(string: "Название:\n",
+                                                         attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 18)])
+        attributedString.append(NSMutableAttributedString(string: events[indexPath.item].name ?? "Отсутствует",
+                                                          attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .regular)]))
+        attributedString.append(NSMutableAttributedString(string: "\nТип:\n",
+                                                          attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18, weight: .bold)]))
+        attributedString.append(NSMutableAttributedString(string: events[indexPath.item].type ?? "Отсутствует",
+                                                          attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .regular)]))
+        attributedString.append(NSMutableAttributedString(string: "\nАдрес:\n",
+                                                          attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18, weight: .bold)]))
+        attributedString.append(NSMutableAttributedString(string: events[indexPath.item].placeAddress ?? "Отсутствует",
+                                                          attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .regular)]))
+        
+        
+        cell.textLabel?.attributedText = attributedString
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "toMyEvent", sender: events[indexPath.item])
+    }
+}
 
