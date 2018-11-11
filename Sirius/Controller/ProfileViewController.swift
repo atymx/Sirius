488f3b33a2f837b81410b6ae97752491f030343d
Maksim <macsim.1999@gmail.com>
2018-11-11 07:38:21 +0300

    profile view

---
+++ Sirius/Controller/ProfileViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ProfileViewController.swift
+//  Sirius
+//
+//  Created by Максим Атюцкий on 11/11/2018.
+//  Copyright © 2018 Atyutskiy. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: - Vars
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
